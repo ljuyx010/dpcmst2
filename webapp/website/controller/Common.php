04f46cd55e307191898a005917b01d2414734193
ljuyx010 <34336224+ljuyx010@users.noreply.github.com>
2018-04-21 17:57:06 +0800

    多图上传

--- webapp/website/controller/Common.php
+++ webapp/website/controller/Common.php
@@ -6,11 +6,13 @@ use org\Upload;
 class Common extends Controller{
 	
 	public function upload (){
-		dump(input('param.'));die;
+		//dump(input('param.'));die;
+		$uproot= 'upload/';
 		$upload = new  \org\Upload();// 实例化上传类
 		$upload->maxSize   =     3145728 ;// 设置附件上传大小
 		$upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
-		$upload->rootPath  =      './upload/images/'; // 设置附件上传目录    // 上传文件
+		$upload->saveExt  = 'jpg'; //设置保存后缀
+		$upload->rootPath  =    './'. $uproot;  // 设置附件上传目录    // 上传文件
 		$upload->saveName=time().rand(1111,9999);
 		$date=date("Y-m-d",time());//已上传日期为子目录名
 		$info   =   $upload->upload();
@@ -19,10 +21,10 @@ class Common extends Controller{
 			$this->error($upload->getError());
 
 		}else{// 上传成功
-			$m=db(images);
-			$data['filepath']='/upload/images/'.$date."/".$upload->saveName.".".$upload->saveExt;
-			$result=$m->add($data);
-			$file=['id'=>$result,'imagepath'=>$data['filepath']];
+			//$m=db(images);
+			$data['filepath']=ROOT .$uproot.$date.'/'.$upload->saveName.'.'.$upload->saveExt;
+			//$result=$m->add($data);
+			$file=['id'=>1,'imagepath'=>$data['filepath']];
 			echo json_encode($file);
 
 		}
